224edfbc96e84eeae9bb0fa4073e74110df2d329
jus2tomate83 <lefebvrebenjamin202@gmail.com>
2023-03-17 22:03:23 +0100

    addon

---
+++ server_scripts/fonction_benj.js
@@ -0,0 +1,16 @@
+//fonction multi smelt
+onEvent('recipes', event => {
+
+    function multiSmelt(type, ingredients_item, results_item, results_chance, processingTime){
+
+
+        
+    }
+
+
+
+    
+
+
+
+})
