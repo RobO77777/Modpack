138302f81c8834814bf4675936c601c7a0439923
jus2tomate83 <lefebvrebenjamin202@gmail.com>
2023-03-18 11:46:19 +0100

    addon function milling

--- server_scripts/fonction_benj.js
+++ server_scripts/fonction_benj.js
@@ -1,10 +1,23 @@
 //fonction multi smelt
 onEvent('recipes', event => {
 
-    function multiSmelt(type, ingredients_item, results_item,processingTime, results_chance){
+    function milling(type, ingredients_item, results_item,processingTime, results_chance){
 
-
-        
+        event.custom({
+            "type": type,
+            "ingredients": [
+              {
+                "item": ingredients_item
+              }
+            ],
+            "results": [
+              {
+                "item": results_item,
+                "chance": results_chance
+              }
+            ],
+            "processingTime": processingTime
+        })
     }
 
 
