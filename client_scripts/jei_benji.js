00f6306e23fe5c5fdf85df0db86326f05ca04e7a
RobO <fr.gautier.fr@gmail.com>
2023-03-19 18:20:05 +0100

    remove patchouli jei

--- client_scripts/jei_benji.js
+++ client_scripts/jei_benji.js
@@ -1,6 +1,7 @@
 onEvent('jei.hide.items', event => {
 
     event.hide(/extendedcrafting/)
+    event.hide(/patchouli/)
 
 }) 
 
@@ -11,15 +12,15 @@ onEvent('jei.add.items', event => {
 
 	event.add([
 		
-        ['extendedcrafting:basic_auto_table',
-         'extendedcrafting:advanced_auto_table',
-         'extendedcrafting:elite_auto_table',
-         'extendedcrafting:ultimate_auto_table',
-         'extendedcrafting:elite_table',
-         'extendedcrafting:basic_table',
-         'extendedcrafting:advanced_table',
-         'extendedcrafting:elite_table',
-         'extendedcrafting:ultimate_table']
+    'extendedcrafting:basic_auto_table',
+    'extendedcrafting:advanced_auto_table',
+    'extendedcrafting:elite_auto_table',
+    'extendedcrafting:ultimate_auto_table',
+    'extendedcrafting:elite_table',
+    'extendedcrafting:basic_table',
+    'extendedcrafting:advanced_table',
+    'extendedcrafting:elite_table',
+    'extendedcrafting:ultimate_table',
 
 	])
   })
