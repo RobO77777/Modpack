2b3ff3b5d15a8dd181283c91dbd9ed0b06d2e38d
jus2tomate83 <lefebvrebenjamin202@gmail.com>
2023-03-19 18:09:43 +0100

    extended  crafting jei

---
+++ client_scripts/jei_benji.js
@@ -0,0 +1,25 @@
+onEvent('jei.hide.items', event => {
+
+    event.hide(/extendedcrafting/)
+
+}) 
+
+
+
+
+onEvent('jei.add.items', event => {
+
+	event.add([
+		
+        ['extendedcrafting:basic_auto_table',
+         'extendedcrafting:advanced_auto_table',
+         'extendedcrafting:elite_auto_table',
+         'extendedcrafting:ultimate_auto_table',
+         'extendedcrafting:elite_table',
+         'extendedcrafting:basic_table',
+         'extendedcrafting:advanced_table',
+         'extendedcrafting:elite_table',
+         'extendedcrafting:ultimate_table']
+
+	])
+  })
