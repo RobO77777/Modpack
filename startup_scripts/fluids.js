160f6e88cd7988f6f9b553774394f9455ed2695a
RobO <fr.gautier.fr@gmail.com>
2023-04-22 17:55:02 +0200

    test fluide

--- startup_scripts/fluids.js
+++ startup_scripts/fluids.js
@@ -1,12 +1,5 @@
-onEvent('fluid.registry', event => {
+events.listen('fluid.registry', e => {
     
-    
-    
-    //event.create('mana_fluid').thinTexture(0x54AFD5).bucketColor(0x54AFD5).displayName('Mana Fluid')
-    
-    
-    //event.create('molten_silicium').thickTexture(0x630097).displayName('Molten Silicium').bucketColor(0x630097)
-    //event.create('molten_thallasium').thickTexture(0x87dad2).displayName('Molten Thallasium').bucketColor(0x87dad2)
-  
+    e.create('molten_insanium').textureThick(0x630097).displayName('Molten Insanium').bucketColor(0x630097)
     
 })
